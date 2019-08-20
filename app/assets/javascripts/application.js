7a98df982b3b1fabe1f11e0af773ec27197edb16
David Touitou <david.touitou.dt@gmail.com>
2019-08-20 23:31:06 +0200

    show avec js sur le prix en fonction du nb de participants

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,12 @@
 //= require rails-ujs
 //= require_tree .
-// const price = document.querySelector(.price);
+  const participants_nb = document.querySelector("#booking_number_of_participants");
+  const calcul = document.querySelector(".calcul");
+  const price = document.querySelector(".price").innerText;
+  const price_wo_currency = parseFloat(price.slice(0, -1));
 
-// price.addEventListener(click, (event) => {
-
-// });
+participants_nb.addEventListener("change", (event) => {
+  const part = participants_nb.value;
+  calcul.innerHTML = part * price_wo_currency;
+  // calcul.insertAdjacentHTML("afterbegin", part * price_wo_currency);
+});
