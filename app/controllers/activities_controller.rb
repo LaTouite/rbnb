9d741115ef3b8a5c68b1883a4c93a691ecd62f4e
theo rouland <theo.rouland@laposte.net>
2019-08-20 19:02:09 +0200

    css improvements in home page

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,8 +1,8 @@
 class ActivitiesController < ApplicationController
-  
   def show
     @activity = Activity.find(params[:id])
     @booking = Booking.new
+  end
 
   def index
     @activities = Activity.all
