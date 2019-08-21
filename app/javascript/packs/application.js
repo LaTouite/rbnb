9eb7ec42193adcce19e7aa4a6e84c1f54faf1731
David Touitou <david.touitou.dt@gmail.com>
2019-08-21 13:38:05 +0200

    js modifié

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,4 +2,20 @@ import "bootstrap";
 import 'mapbox-gl/dist/mapbox-gl.css';
 import { initMapbox } from '../plugins/init_mapbox';
 
+const initPrice = () => {
+  const participants_nb = document.querySelector("#booking_number_of_participants");
+  const calcul = document.querySelector(".calcul");
+  const price = document.querySelector(".price").innerText;
+  const price_wo_currency = parseFloat(price.slice(0, -1));
+
+  participants_nb.addEventListener("change", (event) => {
+    const part = participants_nb.value;
+    calcul.innerHTML = part * price_wo_currency;
+  });
+}
+
+if (document.querySelector("#booking_number_of_participants")) {
+  initPrice();
+}
+
 initMapbox();
