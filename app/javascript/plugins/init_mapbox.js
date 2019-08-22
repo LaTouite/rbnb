e80fc10fb7dde03457abea2302edd6f195a7865c
David Touitou <david.touitou.dt@gmail.com>
2019-08-22 12:32:04 +0200

    show avec map

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,11 +1,13 @@
 import mapboxgl from 'mapbox-gl';
 
+// paramètres du marker
 const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
   map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
 };
 
+// fonction pour géoloc une ou plusieurs activités
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
 
